1b13e4f1d75cc3b0343f545e0e7a79929eb12ed8
Spiylka <shataev.artur@mail.ru>
2025-01-09 02:13:54 +0300

    Выполнил Домашняя работа по уроку "Перегрузка операторов"

--- Module_5_1.py
+++ Module_5_1.py
@@ -11,12 +11,28 @@ class Human:
         self.age += 1
         print(f'У меня день рождения, мне теперь {self.age}')
 
+    def __str__(self):
+        return f'{self.name}'
+
     def __len__(self):
         return self.age
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __bool__(self):
+        return bool(self.age)
 
     def __del__(self):
         print(f'{self.name} ушел')
 
 den = Human('Денис', 22)
-max = Human('Максим', 23)
-print(len(den))
+max = Human('Максим', 22)
+a = 6
+print(den)
--- Перегрузка операторов.py
+++ Перегрузка операторов.py
@@ -1,4 +1,3 @@
-
 # Цель: понять как работают базовые магические методы на практике.
 #
 # Задача "Магические здания":
@@ -42,8 +41,50 @@ class House:
         for i in range(1, new_floor+1):
             print(i)
 
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
     def __len__(self):
         return self.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+            return self.__add__(value)
+
+    def __iadd__(self, value):
+            return self.__add__(value)
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}.'
@@ -52,10 +93,22 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
+print(h1 == h2) # __eq__
 
-# __len__
-print(len(h1))
-print(len(h2))
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
